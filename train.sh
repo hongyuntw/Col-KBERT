23f8dc0c762528459e972f4a9d2526824afd3ab3
hongyuntw <harrychiang0@gmail.com>
2021-04-24 15:00:52 +0800

    remove ignored file

--- train.sh
+++
@@ -1,5 +0,0 @@
-yes yes | python3 -m colbert.train --amp --doc_maxlen 180 --mask-punctuation --bsize 16 --accum 1 \
---triples /home/nlp/GeneralQA/MSMARCO/passage/triples.train.small.tsv \
---resume --resume_optimizer \
---checkpoint /home/nlp/GeneralQA/Col-KBERT/result/msmarco/col_kbert_v2/train.py/msmarco.psg.l2.col_kbert_v2/checkpoints/colbert-300000.dnn \
---root result/msmarco --experiment col_kbert_v2 --similarity l2 --run msmarco.psg.l2.col_kbert_v2
--- validate.sh
+++
@@ -1,5 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python3 -m colbert.test --amp --doc_maxlen 180 --mask-punctuation --bsize 1024 \
---topk /home/nlp/GeneralQA/MSMARCO/passage/top1000.dev  \
---qrels /home/nlp/GeneralQA/MSMARCO/passage/qrels.dev.small.tsv \
---checkpoint /home/nlp/GeneralQA/Col-KBERT/result/msmarco/col_kbert_v2/train.py/msmarco.psg.l2.col_kbert_v2/checkpoints/colbert-300000.dnn \
---root result/msmarco --experiment col_kbert_v2
